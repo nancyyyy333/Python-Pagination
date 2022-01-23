81a5d5893f5cbcccf124056f2439f7540d2c0f7c
nancyyyy333 <nancywu6666@gmail.com>
2022-01-23 21:16:32 +0800

    Enter

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ def pagination(max_page,curr_page,index):
     sum = index//2
     #print the exception at the start
     if int(curr_page) < (index):
-        print([*range(1,index+1)])
+        return([*range(1,index+1)])
     #print the exception at the end 
     elif curr_page > (max_page - index):
-        print([*range((max_page+1)-index, max_page+1)])
+        return([*range((max_page+1)-index, max_page+1)])
     else:
         if index%2 == 0:
-            print([*range(curr_page-sum,curr_page+sum)])
+            return([*range(curr_page-sum,curr_page+sum)])
         else:
-            print([*range(curr_page-sum,curr_page+sum+1)])
+            return([*range(curr_page-sum,curr_page+sum+1)])
+
